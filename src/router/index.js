86cee79abae237bc8cea74a8ffb6585e0f0a0560
Simon <simimueller@hotmail.com>
2020-11-11 11:31:31 +0100

    add mapbox view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import MapView from "../views/MapView.vue";
 
 const routes = [
     {
@@ -15,6 +16,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () =>
             import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/map",
+        name: "Map",
+        component: MapView,
     },
 ];
 
---
+++ src/views/MapView.vue
@@ -0,0 +1,42 @@
+<template>
+    <link
+        href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+        rel="stylesheet"
+    />
+    <h2>My Mapbox</h2>
+    <div id="map"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import mapboxgl from "mapbox-gl";
+
+export default {
+    name: "Map",
+    data: function() {},
+    components: {},
+    mounted: function() {
+        mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
+        new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/seimon/ckh3f75gc09mu19nvgrieu4dj", // stylesheet location
+            center: [8.30635, 47.05048], // starting position [lng, lat]
+            zoom: 13, // starting zoom
+        });
+    },
+};
+</script>
+
+<style scoped>
+body {
+    margin: 0;
+    padding: 0;
+}
+#map {
+    position: absolute;
+    top: 20;
+    bottom: 0;
+    width: 100%;
+    height: 80%;
+}
+</style>
